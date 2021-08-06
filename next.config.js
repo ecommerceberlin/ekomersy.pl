3342f08bec0331cb61f7460a1d5ccf7fc07f78fa
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-08-06 14:41:43 +0200

    updated base libs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 
 const path = require('path');
 const withTM = require('next-transpile-modules')(['eventjuicer-site-components']);
+const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = withTM({
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+
+
+module.exports = withSentryConfig(withTM({
 
     webpack: (config) => {
       config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
@@ -14,4 +29,4 @@ module.exports = withTM({
       defaultLocale: 'pl',    
     },
   
-});
+}), SentryWebpackPluginOptions);
